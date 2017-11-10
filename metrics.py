409a9b2db175c2d87b8728db138d84b44bab6611
Caio Rocha Pereira <crp3@cin.ufpe.br>
2017-11-10 18:51:39 -0300

    adds adjusted Q

--- metrics.py
+++ metrics.py
@@ -10,12 +10,12 @@ def covariance(img1, img2):
         for j in range(N):
             sum_ += (img1[i][j] - xmean)*(img2[i][j] - ymean)
 
-    return sum_/(M*N)
+    return sum_/((M*N) - 1)
 
 def loss_correl(img1, img2):
     cov = covariance(img1, img2)
-    cov = cov/(covariance(img1,img1) * covariance(img2,img2))
-    return cov
+    a = cov/(covariance(img1,img1) * covariance(img2,img2))
+    return a
 
 def lum_distortion(img1, img2):
     xmean = np.mean(img1)
@@ -38,6 +38,10 @@ def Q(img1, img2):
     cd = contrast_distortion(img1, img2)
     return lc*ld*cd
 
+def Q2(img1, img2):
+    top = 4*covariance(img1,img2)*np.mean(img1)*np.mean(img2)
+    bot = (covariance(img1, img1) + covariance(img2,img2)) * ((np.mean(img1)**2) + (np.mean(img2)**2))
+    return top/bot
 def mse(img1, img2):
     M, N = img1.shape
     sum_ = 0
@@ -49,4 +53,4 @@ def mse(img1, img2):
 
 img = cv2.imread('imgs/Image42.png', 0)
 blur = cv2.imread('imgs/blur.png', 0)
-print(Q(img, img))
+print(Q2(img, img))
