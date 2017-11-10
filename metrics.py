085ffa26f3a7b18dd2c7e6453a69613a33ff60e9
Caio Rocha Pereira <crp3@cin.ufpe.br>
2017-11-10 18:41:26 -0300

    adds bugged Q

--- metrics.py
+++ metrics.py
@@ -12,9 +12,31 @@ def covariance(img1, img2):
 
     return sum_/(M*N)
 
+def loss_correl(img1, img2):
+    cov = covariance(img1, img2)
+    cov = cov/(covariance(img1,img1) * covariance(img2,img2))
+
+def lum_distortion(img1, img2):
+    xmean = np.mean(img1)
+    ymean = np.mean(img2)
+    top = 2*xmean*ymean
+    bot = (xmean**2) + (ymean**2)
+    return top/bot
+
+def contrast_distortion(img1,img2):
+    xvar = covariance(img1, img1)
+    yvar = covariance(img2, img2)
+    top = xvar * yvar * 2
+    bot = (xvar**2) + (yvar**2)
+    return top/bot
+
+
 def Q(img1, img2):
-    M, N = img1.shape
-    return M
+    lc = loss_correl(img1, img2)
+    ld = lum_distortion(img1, img2)
+    cd = contrast_distortion(img1, img2)
+    print(lc, ld, cd)
+    return lc*ld*cd
 
 def mse(img1, img2):
     M, N = img1.shape
@@ -27,4 +49,4 @@ def mse(img1, img2):
 
 img = cv2.imread('imgs/Image42.png', 0)
 blur = cv2.imread('imgs/blur.png', 0)
-print(covariance(img, blur))
+print(Q(img, blur))
