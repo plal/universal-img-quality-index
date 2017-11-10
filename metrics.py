a3ee8d574b25535b5c3fbebecfbe96da20fa505a
Caio Rocha Pereira <crp3@cin.ufpe.br>
2017-11-10 18:28:34 -0300

    adds covariance

--- metrics.py
+++ metrics.py
@@ -1,7 +1,16 @@
 import cv2
 import numpy as np
 
+def covariance(img1, img2):
+    xmean = np.mean(img1)
+    ymean = np.mean(img2)
+    M, N = img1.shape
+    sum_ = 0
+    for i in range(M):
+        for j in range(N):
+            sum_ += (img1[i][j] - xmean)*(img2[i][j] - ymean)
 
+    return sum_/(M*N)
 
 def Q(img1, img2):
     M, N = img1.shape
@@ -12,10 +21,10 @@ def mse(img1, img2):
     sum_ = 0
     for i in range(M):
         for j in range(N):
-            sum_ += (img1[i][j] - img2[i][j])**2/(M*N)
+            sum_ += (img1[i][j] - img2[i][j])**2
 
-    return sum_
+    return sum_/(M*N)
 
 img = cv2.imread('imgs/Image42.png', 0)
 blur = cv2.imread('imgs/blur.png', 0)
-print(mse(img,blur))
+print(covariance(img, blur))
